a7ac4ff6f8d02bfc1f20ac6826bfe7b541006b4a
Pninja12 <74876860+Pninja12@users.noreply.github.com>
2023-03-31 19:44:42 +0100

    Random Dice concluído

---
+++ RandomDice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RandomDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Console.Write("Quantos dados?  -> ");
+            string string_dados = Console.ReadLine();
+            int lancamento = int.Parse(string_dados);
+            int dado = 0;
+
+            for (int i = 0; i < lancamento; i++)
+            {
+                dado = dado + rnd.Next(1, 7);
+            }
+
+            Console.Write($"O resultado é {dado}");
+        }
+    }
+}
