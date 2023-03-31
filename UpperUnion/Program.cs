c2f774689f791669a6d850cbcf25912dccfd6a55
Pninja12 <74876860+Pninja12@users.noreply.github.com>
2023-03-31 21:54:56 +0100

    Exercicio de sexta feito, apenas não consegui implementar todas as letra maiusculas

---
+++ UpperUnion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UpperUnion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if(args.Length <= 0)
+            {
+                Console.WriteLine("NO ARGUMENTS");   
+            }
+            else
+            {
+
+                for (int i = 0; i < args.Length; i++)
+                {
+                    Console.Write(args[i]);
+                    Console.Write("-");
+                }
+            } 
+        }
+    }
+}
